65a032c827a6212a256872ed95105f7267075ffd
tuando95 <69784017+tuando95@users.noreply.github.com>
2023-07-07 09:28:43 +0700

    update advanced

---
+++ programing-with-python/advanced-python/02-abstractions-with-data/hw1.py
@@ -0,0 +1,60 @@
+# Treat all the following code as being behind an abstraction layer, you shouldn't need to look at it!
+
+
+def make_city(name, lat, lon):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    >>> get_lat(city)
+    0
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return {"name": name, "lat": lat, "lon": lon}
+    else:
+        return [name, lat, lon]
+
+
+def get_name(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    """
+    if change_abstraction.changed:
+        return city["name"]
+    else:
+        return city[0]
+
+
+def get_lat(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lat(city)
+    0
+    """
+    if change_abstraction.changed:
+        return city["lat"]
+    else:
+        return city[1]
+
+
+def get_lon(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return city["lon"]
+    else:
+        return city[2]
+
+
+def change_abstraction(change):
+    change_abstraction.changed = change
+
+
+change_abstraction.changed = False
